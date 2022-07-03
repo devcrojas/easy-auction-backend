bf39a520e1f91e21e603589603a3526bc9339225
jc.juarezcampos <213271027@alumnos.utn.edu.mx>
2022-07-02 19:14:21 -0500

    modificacion de peticion de perfil

--- routes/index.js
+++ routes/index.js
@@ -65,7 +65,7 @@ router.post("/login", async function (req, res, next) {
           name: login[0].name,
           id: login[0]._id,
           date: new Date(),
-          profile: profile
+          profile: profile[0]
         }, process.env.TOKEN_SECRET)
 
         res.header('auth-token', token).json({
