770f9bd87f5e4040c5d29efc54cabb2a48e701b0
AlanJair <alanabc.ag@gmail.com>
2022-07-26 00:49:46 +0200

    Endpoint comentado borrado

--- routes/profiles.js
+++ routes/profiles.js
@@ -110,42 +110,6 @@ router.post('/', multer.upload.single('file'), async (req, res, next) => {
   }
 });
 
-// ACTUALIZAR perfil
-/*router.put('/:id', multer.upload.single('file'), async (req, res, next) => {
-   jwt.verify(req.body.token, process.env.TOKEN_SECRET, async (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
-    } else {
-      //console.log(user);
-      try{
-        //console.log(user.profile);
-        let token = jwt.decode(req.body.token);
-        const updateProfile = {
-          firstName:user.profile.firstName,
-          lastName:user.profile.lastName,
-          birthday:user.profile.birthday,
-          address:{
-            cpp:user.profile.address.cpp,
-            street:user.profile.address.street,
-            suburb:user.profile.address.suburb,
-            municipaly:user.profile.address.municipaly,
-            state:user.profile.address.state
-          },
-          phone:user.profile.phone,
-          email:user.profile.email
-        };
-    
-        await Profile.findByIdAndUpdate(req.params.id, updateProfile);
-        const getProfile = await Profile.findById(req.params.id);
-        token.profile = getProfile;
-        let newToken = jwt.sign(token, process.env.TOKEN_SECRET);
-        res.status(201).send(newToken);
-      }catch(error) {
-        res.status(400).send(error.message);
-      }
-    }
-  });
-}); */
 router.put('/:id', multer.upload.single('file'), async (req, res, next) => {
   try {
     //console.log(req.body);
